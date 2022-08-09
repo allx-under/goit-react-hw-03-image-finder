68f39c68b8e248ad119a9bd16208855090b75b2a
allx-under <95172339+allx-under@users.noreply.github.com>
2022-08-09 13:03:41 +0300

    add start of hw3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { SearchBar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import { Component } from 'react';
+
+import { API } from './API/API';
+
+const imgAPI = new API();
+
+export class App extends Component {
+  state = {
+    input: '',
+  };
+
+  onSubmit = async e => {
+    e.preventDefault();
+
+    const inputText = e.target.elements.input.value;
+    this.setState({ input: inputText });
+
+    const { hits } = await imgAPI.fetchImgs(this.state.input);
+
+    e.target.reset();
+  };
+
+  render() {
+    return (
+      <>
+        <SearchBar onSubmit={this.onSubmit} />
+        <p>{this.state.input} </p>
+        <ImageGallery>
+          <ImageGalleryItem />
+        </ImageGallery>
+      </>
+    );
+  }
+}
